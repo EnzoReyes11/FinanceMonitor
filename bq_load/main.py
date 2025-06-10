7786a54593343c04c63d334707febfa0430763e5
Enzo Reyes <reyes.enzo1985@gmail.com>
2025-06-09 22:42:35 -0300

    [ADD] Accepting POST symbols data

--- bq_load/main.py
+++ bq_load/main.py
@@ -1,9 +1,11 @@
 import io
 import logging
 import os
+from datetime import datetime
 
 import functions_framework
 from dotenv import load_dotenv
+from flask import jsonify
 from google.cloud import bigquery
 
 load_dotenv()
@@ -17,10 +19,10 @@ logger = logging.getLogger(__name__)
 
 @functions_framework.http
 def bq_load_data(request):
-    if request.method != "POST":
-        logger.error("Only POST requests are accepted")
+    if request.method != "POST" or request.content_type != "application/json":
+        logger.error("Only POST/json requests are accepted")
 
-        return "Only POST requests are accepted", 405
+        return jsonify({"error": "Only POST/json requests are accepted."}), 405
 
     BQ_PROJECT_ID = os.getenv("BQ_PROJECT_ID")
     BQ_DATASET_ID = os.getenv("BQ_DATASET_ID")
@@ -31,15 +33,48 @@ def bq_load_data(request):
             "Missing environment variables (BQ_PROJECT_ID, BQ_DATASET_ID, BQ_TABLE_ID)."
         )
 
-        return "Server configuration error.", 500
+        return jsonify({"error": "Internal Server Error."}), 500
 
+    data = request.get_json()
+    if not data or "symbols" not in data:
+        logger.error("Invalid request payload, missing symbols data.")
+
+        return jsonify({"error": "Invalid request payload."}), 400
+
+    symbols = data["symbols"]
     try:
-        file_contents = "SPY,700,US,2025-06-09T16:42:31.190280"
-        fake_file = io.StringIO(file_contents)
-    except Exception:
-        logger.exception("Error creating fake file:")
+        for symbol in symbols:
+            if len(symbol) != 4:
+                error_message = "Invalid symbol structure: %s. Expected 4 elements." % (
+                    symbol,
+                )
+                logger.error(error_message)
 
-        return "Internal server error.", 500
+                return jsonify(
+                    {"error": "Missing parameters.", "details": error_message}
+                ), 400
+
+            try:
+                date_string = symbol[3]
+                datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S")
+            except ValueError:
+                safe_error_detail = (
+                    "The provided date '%s' does not match the required format 'YYYY-MM-DDTHH:MM:SS'."
+                    % (date_string,)
+                )
+                logger.exception(safe_error_detail)
+
+                return jsonify(
+                    {"error": "Invalid date format.", "details": safe_error_detail}
+                ), 400
+
+            file_contents = "%s,%s,%s,%s" % (symbol[0], symbol[1], symbol[2], symbol[3])
+            fake_file = io.StringIO(file_contents)
+
+    except Exception:
+        logger.exception("An unexpected error occurred during symbol processing:")
+
+        return jsonify({"error": "Internal Server Error."}), 500
 
     try:
         client = bigquery.Client()
@@ -54,7 +89,7 @@ def bq_load_data(request):
 
         job.result()
 
-        table = client.get_table(table_id)  # Make an API request.
+        table = client.get_table(table_id)
         print(
             "Loaded {} rows and {} columns to {}".format(
                 table.num_rows, len(table.schema), table_id
@@ -62,6 +97,7 @@ def bq_load_data(request):
         )
     except Exception:
         logger.exception("Error loading data into BigQuery:")
-        return "Internal server error.", 500
+
+        return jsonify({"error": "Internal Server Error."}), 500
 
     return "Data loaded successfully.", 200
